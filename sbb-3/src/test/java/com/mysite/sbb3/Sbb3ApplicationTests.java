d6db176c364bea86e93eb604e09fe703a61b1724
mk-404lab <alsgur9984@naver.com>
2026-03-18 20:54:06 +0900

    feat: 3-02 페이징 기능 구현

--- sbb-3/src/test/java/com/mysite/sbb3/Sbb3ApplicationTests.java
+++ sbb-3/src/test/java/com/mysite/sbb3/Sbb3ApplicationTests.java
@@ -3,6 +3,7 @@ package com.mysite.sbb3;
 import com.mysite.sbb3.answer.Answer;
 import com.mysite.sbb3.question.Question;
 import com.mysite.sbb3.question.QuestionRepository;
+import com.mysite.sbb3.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,18 +21,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class Sbb3ApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
-    @Transactional
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        List<Answer> answerList = q.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
     }
 }
